d9edf16c0f5411366a46b4025234934e4b9e6115
Andrew Clifton <clifton.arc@gmail.com>
2022-01-15 16:37:29 +0000

    Add reverse rows and smooth position options to arrange

--- ComponentUtils.lua
+++ ComponentUtils.lua
@@ -9,6 +9,9 @@ function ComponentUtils.arrange(params)
         padding = .1,
         rows = 0,
         cols = 0,
+        positionSmooth = false,
+        positionFast = false,
+        reverseRowDirection = false,
     }
     for k,v in pairs(params) do values[k] = v end
     if #values.items == 0 then return end
@@ -27,24 +30,31 @@ function ComponentUtils.arrange(params)
     local rows = values.rows
     local cols = values.cols    
     for _, item in ipairs(values.items) do
+        item.setRotation(values.rotation)
         max_item_bounds = Vector.max(max_item_bounds, item.getBounds().size)
     end
     max_item_bounds.y = 0
     local first_position = position
-    local padding_initial_offset = Vector(padding[1]*(cols-1)/2, 0, padding[3]*(rows-1)/2)
     if values.alignment == "center" then
-        first_position = first_position - Vector(
-            max_item_bounds[1]*(cols-1)/2 + padding[1]*(cols-1)/2,
+        local originOffset = Vector(
+            -max_item_bounds[1]*(cols-1)/2 - padding[1]*(cols-1)/2,
             0,
             max_item_bounds[3]*(rows-1)/2 + padding[3]*(rows-1)/2
         )
+        if values.reverseRowDirection then
+            originOffset.z = -originOffset.z
+        end
+        first_position = first_position + originOffset
     elseif values.alignment == "left" then
     else
         error("Invalid arg alignment:"..values.alignment)
     end
 
     local positionOffsetX = Vector(max_item_bounds[1] + padding[1], 0, 0)
-    local positionOffsetZ = Vector(0, 0, max_item_bounds[3] + padding[3])
+    local positionOffsetZ = -1 * Vector(0, 0, max_item_bounds[3] + padding[3])
+    if values.reverseRowDirection then
+        positionOffsetZ = -1 * positionOffsetZ
+    end
     for i, item in ipairs(values.items) do
         local index = i - 1
         local r = math.floor(index/cols)
@@ -52,8 +62,12 @@ function ComponentUtils.arrange(params)
         local current = first_position + positionOffsetX * (c) + positionOffsetZ * (r)
         item.setVelocity({0,0,0})
         item.setAngularVelocity({0,0,0})
-        item.setPosition(current)
         item.setRotation(values.rotation)
+        if values.positionSmooth then
+            item.setPositionSmooth(current, false, values.positionFast)
+        else
+            item.setPosition(current)
+        end
     end    
 end
 
